21570065fb38d3a85a60d8726028d81b0bcdc5a0
Shu Fujita <osorubeki.fujita@gmail.com>
2016-02-05 21:42:57 +0900

    20160205_214300 Update - [Helper] Nard::Rails::FormHelper#showing_field

--- app/helpers/nard/rails/form_helper.rb
+++ app/helpers/nard/rails/form_helper.rb
@@ -6,4 +6,14 @@ module Nard::Rails::FormHelper
     }
   end
 
+  def showing_field( label_name, info, grid: { sm: 3 } )
+    label_grid_classes = grid.map { |k,v| "col-#{k}-#{v}" }
+    info_grid_classes = grid.map { |k,v| "col-#{k}-#{ 12 - v }" }
+
+    content_tag( :div, class: [ :field, :row ] ) {
+      concat content_tag( :div, label_name, class: [ 'showing-label', label_grid_classes ].flatten )
+      concat content_tag( :div, info, class: info_grid_classes )
+    }
+  end
+
 end
