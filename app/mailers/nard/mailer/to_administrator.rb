0515db8bc966973516334e98275371e0e0abc1cf
Shu Fujita <osorubeki.fujita@gmail.com>
2016-02-07 22:25:29 +0900

    20160207_222530 Update - [Mailer] Default address settings

--- app/mailers/nard/mailer/to_administrator.rb
+++ app/mailers/nard/mailer/to_administrator.rb
@@ -1,16 +1,16 @@
 class Nard::Mailer::ToAdministrator < Nard::Mailer::Master
 
   default from: Settings::Static.action_mailer.main_email_address
+  default to: Settings::Static.action_mailer.main_email_address
 
   def as_to_contact( contact )
     @contact = contact
 
     set_containers_as_to_contact
 
-    send_to = Settings::Static.action_mailer.main_email_address
     subject = "【#{Settings::Static.app_title}】お問い合わせがありました"
 
-    mail( to: send_to , subject: subject ) do | format |
+    mail( subject: subject ) do | format |
       format.html { render 'mailer/nard/rails/to_administrator/as_to_contact' }
     end
   end
--- app/mailers/nard/mailer/to_system_administrator.rb
+++ app/mailers/nard/mailer/to_system_administrator.rb
@@ -1,15 +1,14 @@
 class Nard::Mailer::ToSystemAdministrator < Nard::Mailer::Master
 
-  default from: Settings::Static.action_mailer.system_admin_email_address
+  default from: Settings::Static.action_mailer.main_email_address
+  default to: Settings::Static.action_mailer.system_admin_email_address
 
   def as_to_error( error_data )
     @error_data = error_data
 
-    send_to = Settings::Static.action_mailer.system_admin_email_address
-    send_from = Settings::Static.action_mailer.main_email_address
     subject = "【#{Settings::Static.app_title}】エラー発生"
 
-    mail( to: send_to, from: send_from , subject: subject ) do | format |
+    mail( subject: subject ) do | format |
       format.html { render 'mailer/nard/rails/to_system_administrator/as_to_error' }
     end
   end
