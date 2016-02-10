a06ee414fd5404d951b3e815c9ed1f5314f6aeaa
Shu Fujita <osorubeki.fujita@gmail.com>
2016-02-10 21:51:13 +0900

    20160210_215120 Update - [Helper] Add methods to Nard::Rails::LinkHelper

--- app/helpers/nard/rails/link_helper.rb
+++ app/helpers/nard/rails/link_helper.rb
@@ -10,4 +10,16 @@ module Nard::Rails::LinkHelper
   alias :open_on_another_window :display_on_another_window
   alias :open_on_new_window :display_on_another_window
 
+  def route_info_path( request )
+    "#{ current_host( request ) }/rails/info/routes"
+  end
+
+  def current_host( request )
+    request[ 'host' ]
+  end
+
+  def url_regexp
+    /\Ahttps?\:\/{2}/
+  end
+
 end
