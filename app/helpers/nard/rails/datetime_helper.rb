cbbc10a184478b7e0f17354a2a3170fbe39d5410
Shu Fujita <osorubeki.fujita@gmail.com>
2016-02-17 08:54:30 +0900

    20160217_085430 Update - [Helper] Nard::Rails::DatetimeHelper

--- app/helpers/nard/rails/datetime_helper.rb
+++ app/helpers/nard/rails/datetime_helper.rb
@@ -1,7 +1,15 @@
 module Nard::Rails::DatetimeHelper
 
-  def date_ja_normal( date )
-    "#{ date.year }年#{ date.month }月#{ date.day }日"
+  def date_ja_normal( date, day_name: false )
+    str = "#{ date.year }年#{ date.month }月#{ date.day }日"
+    str += "（#{ l( date, format: :day_name ) }）" if day_name
+    str
+  end
+
+  def date_ja_without_year( date, day_name: false )
+    str = "#{ date.month }月#{ date.day }日"
+    str += "（#{ l( date, format: :day_name ) }）" if day_name
+    str
   end
 
 end
