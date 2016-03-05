4888447b8ec44adb9f2810fab13ce3b5524756c1
Shu Fujita <osorubeki.fujita@gmail.com>
2016-03-05 17:05:35 +0900

    20160305_170540 Update - [Service] Add Nard::Rails::StatusHandler

--- app/services/nard/rails/session_handler.rb
+++ app/services/nard/rails/session_handler.rb
@@ -1,28 +1,8 @@
 # Session の処理を行うクラス
-class Nard::Rails::SessionHandler
+class Nard::Rails::SessionHandler < Nard::Rails::StatusHandler
 
   def initialize( session )
     @session = session
-  end
-
-  def get(k)
-    @session[ k.to_s ]
-  end
-
-  def set( k , v )
-    @session[ k.to_s ] = v
-  end
-
-  def fetch(k)
-    v = get(k)
-    delete!(k) unless v.nil?
-    v
-  end
-
-  def delete!( *args )
-    [ args ].flatten.each do | key |
-      set( key, nil ) if get( key ).present?
-    end
   end
 
   def reset!( h = { unless: [ :session_id, :_csrf_token , "warden.user.user.key" ] } )
@@ -34,24 +14,16 @@ class Nard::Rails::SessionHandler
     delete!( *args )
   end
 
-  def reset_all!
-    delete!( keys )
-  end
-
   def delegate_attributes( of:, to: )
     record, session_key = to, of
     v = fetch( session_key )
     record.assign_attributes(v) if v.present?
   end
 
-  def keys
-    @session.keys
-  end
-
   private
 
-  def method_missing( method_name , *args )
-    @session.send( method_name , *args )
+  def obj
+    @session
   end
 
 end
---
+++ app/services/nard/rails/status_handler.rb
@@ -0,0 +1,37 @@
+class Nard::Rails::StatusHandler
+
+  def get(k)
+    obj[ k.to_s ]
+  end
+
+  def set( k , v )
+    obj[ k.to_s ] = v
+  end
+
+  def fetch(k)
+    v = get(k)
+    delete!(k) unless v.nil?
+    v
+  end
+
+  def delete!( *args )
+    [ args ].flatten.each do | key |
+      set( key, nil ) if get( key ).present?
+    end
+  end
+
+  def reset_all!
+    delete!( keys )
+  end
+
+  def keys
+    obj.keys
+  end
+
+  private
+
+  def method_missing( method_name , *args )
+    obj.send( method_name , *args )
+  end
+
+end
