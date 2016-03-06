66607677b06056b6f474a759975ca077423541bf
Shu Fujita <osorubeki.fujita@gmail.com>
2016-03-06 13:04:46 +0900

    20160306_130450 Clean - [ControllerExt] Nard::Rails::ControllerExt::Request

--- lib/nard_rails/controller_ext/request.rb
+++ lib/nard_rails/controller_ext/request.rb
@@ -6,13 +6,9 @@ module Nard::Rails::ControllerExt::Request
     request.referrer
   end
 
-  alias :referer :referrer
-
   def referrer_recognized
     @referrer_recognized ||= Rails.application.routes.recognize_path( request.referrer )
   end
-
-  alias :referer_recognized :referrer_recognized
 
   def referrer_url
     url_for( referrer_recognized )
@@ -22,6 +18,8 @@ module Nard::Rails::ControllerExt::Request
     url_for(h)
   end
 
+  alias :referer :referrer
+  alias :referer_recognized :referrer_recognized
   alias :referer_url :referrer_url
 
 end
