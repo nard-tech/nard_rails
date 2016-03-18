7442e25e74a29737feeeb949a58c9c4031757e8c
Shu Fujita <osorubeki.fujita@gmail.com>
2016-03-18 16:50:10 +0900

    20160318_165020 Update - [Controller] Add Nard::Rails::AjaxController

---
+++ app/controllers/nard/rails/ajax_controller.rb
@@ -0,0 +1,11 @@
+class Nard::Rails::AjaxController < Nard::Rails::AppController
+
+  respond_to :json
+  before_action :redirect_unless_xhr
+
+  protect_from_forgery with: :null_session
+
+  include Nard::Rails::ControllerExt::Ability
+  include Nard::Rails::ControllerExt::Ajax::Authentication
+
+end
