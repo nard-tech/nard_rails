fdb65588fc964deaf252988c10585f9d52a5211c
Shu Fujita <osorubeki.fujita@gmail.com>
2016-04-06 14:58:35 +0900

    20160406_145830 Bug fix - [ControllerExt] Nard::Rails::ControllerExt::Json

--- lib/nard_rails/controller_ext/json.rb
+++ lib/nard_rails/controller_ext/json.rb
@@ -21,21 +21,14 @@ module Nard::Rails::ControllerExt::Json
 
   alias :redirect_if_json :reject_if_json
 
+  # @note Json を受け付けない場合は、Json でエラーを返す。
   def rescue_if_json
     set_flash_alert_if_json
-    redirect_to( redirect_path_if_json( params[:action] ), format: :html, status: :unsupported_media_type )
+    render( json: {}, status: :unsupported_media_type )
     return
   end
 
-  def set_flash_alert_if_json
-    flash[ :alert ] = '不正なアクセスを検出しました。'
-  end
-
-  def redirect_path_if_json( action = nil )
-    main_app.root_path
-  end
-
-  # @!group Unless Json - Json のみを受け付ける場合
+  # @!group Unless Json - Json 以外を受け付けない場合
 
   def reject_unless_json
     raise Nard::Rails::Controller::NotJsonError unless json_request?
@@ -43,9 +36,19 @@ module Nard::Rails::ControllerExt::Json
 
   alias :redirect_unless_json :reject_unless_json
 
+  # @note Json 以外を受け付けない場合は、Html でエラーを返す。
   def rescue_unless_json
-    render( json: {}, status: :unsupported_media_type )
+    set_flash_alert_unless_json
+    redirect_to( redirect_path_unless_json, format: :html, status: :unsupported_media_type )
     return
+  end
+
+  def set_flash_alert_unless_json
+    flash[ :alert ] = '不正なアクセスを検出しました。'
+  end
+
+  def redirect_path_unless_json
+    main_app.root_path
   end
 
   # @!endgroup
