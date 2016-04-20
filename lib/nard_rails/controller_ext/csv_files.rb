7f6cb4a37c60cce8d75382a3489e34c55627cdc3
Shu Fujita <osorubeki.fujita@gmail.com>
2016-04-20 19:01:18 +0900

    20160420_190130 Update - [ControllerExt] Add Nard::Rails::ControllerExt::CsvFiles#set_csv_generator

--- lib/nard_rails/controller_ext/csv_files.rb
+++ lib/nard_rails/controller_ext/csv_files.rb
@@ -1,8 +1,30 @@
 # Csv 出力を行うメソッドを格納するモジュール
 module Nard::Rails::ControllerExt::CsvFiles
 
+  extend ActiveSupport::Concern
+
+  included do
+
+    # @!group Filters before action - Set variables (collection - csv)
+
+    before_action :set_csv_generator, only: :csv
+
+    # @!endgroup
+
+  end
+
   def csv
     send_data(@csv_generator.render, type: @csv_generator.type, filename: @csv_generator.filename)
   end
+
+  private
+
+    # @!group Before action - Set variables (collection - csv)
+
+  def set_csv_generator
+    @csv_generator = DrsCloud::CsvGenerator.new( all_collection )
+  end
+
+  # @!endgroup
 
 end
