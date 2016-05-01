3f734f2f25ccc0dda52f45e46e34d8e5a24586c3
Shu Fujita <osorubeki.fujita@gmail.com>
2016-05-01 20:42:37 +0900

    20160501_204230 Update - [DecoratorExt] Add Nard::Rails::DecoratorExt::List.render_cell_in_header_of_list

--- lib/nard_rails/decorator_ext/list.rb
+++ lib/nard_rails/decorator_ext/list.rb
@@ -7,22 +7,43 @@ module Nard::Rails::DecoratorExt::List
 
     # @!group Class methods - Rendering
 
-    def render_header_of_list(btns_in_list: true)
-      h.content_tag(:thead) {
-        h.content_tag(:tr, class: 'list-tbl__header') {
+    def render_header_of_list( search_engine = nil, btns_in_list: true, rowspan: nil )
+      h.content_tag( :thead ) {
+        first_row = h.content_tag(:tr, class: 'list-tbl__header') {
           ary = []
 
           self::INFOS_ON_LIST_TABLE.each do |column, v|
-            d = ::Nard::Rails::DecoratorExt::CellDecoration::Html::List::InHeaderRow.new(v, self, column)
-            ary << h.content_tag(:th, d.label, class: d.class_names('column-name'))
+            ary << render_cell_in_header_of_list( v, column, search_engine )
           end
+
           if btns_in_list
-            ary << h.content_tag(:th, '', class: 'btns-column')
+            ary << h.content_tag(:th, '', class: 'btns-column', rowspan: rowspan )
           end
 
           ary.join.html_safe
         }
+
+        if rowspan.present?
+          tr_ary = []
+          tr_ary << first_row
+
+          yield( tr_ary ) if block_given?
+
+          tr_ary.join.html_safe
+        else
+          first_row
+        end
       }
+    end
+
+    private
+
+    def render_cell_in_header_of_list( v, column, search_engine = nil )
+      d = ::Nard::Rails::DecoratorExt::CellDecoration::Html::List::InHeaderRow.new(v, self, column)
+      th_content = ( d.with_sort_link? ? h.sort_link( search_engine, d.attribute, d.label ) : d.label )
+      options = { class: d.class_names('column-name') }
+
+      h.content_tag( :th, th_content, options )
     end
 
     # @!endgroup
