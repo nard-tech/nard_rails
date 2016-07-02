6c208b0a92a4d885222a9e2c9d27abcc61d5f896
Shu Fujita <osorubeki.fujita@gmail.com>
2016-07-03 04:09:37 +0900

    20160703_040940 Refactor - [Helper] Nard::Rails::ButtonHelper#basic_button

--- app/helpers/nard/rails/button_helper.rb
+++ app/helpers/nard/rails/button_helper.rb
@@ -1,8 +1,8 @@
 module Nard::Rails::ButtonHelper
 
-  def basic_button( tag_type, title = nil, btn_class: nil, btn_id: nil, path: nil, target: nil, layout_type: :h, icon: nil, icon_size: nil, method_of_link: nil, data_attr_of_link: nil, form: nil, children: nil, hidden: false, additional_btn_classes: nil, data_attributes: nil )
+  def basic_button( tag_type, text = nil, btn_class: nil, btn_id: nil, path: nil, target: nil, layout_type: :h, icon: nil, icon_size: nil, method_of_link: nil, data_attr_of_link: nil, form: nil, children: nil, hidden: false, additional_btn_classes: nil, data_attributes: nil )
     raise ArgumentError unless [ 'div', 'submit', 'submit_button' ].include?(tag_type.to_s)
-    raise ArgumentError if title.present? and !( title.kind_of?( String ) or title.instance_of?( Symbol ) )
+    raise ArgumentError if text.present? and !( text.kind_of?( String ) or text.instance_of?( Symbol ) )
     if btn_class.present?
       raise ArgumentError unless btn_class.instance_of?( String ) or btn_class.instance_of?( Symbol )
     end
@@ -24,9 +24,9 @@ module Nard::Rails::ButtonHelper
     if icon.present?
       icon_classes = [ icon.to_s.dasherize, 'fw' ]
       icon_classes << "#{ icon_size }x" if icon_size.present?
-      icon_and_text_html = fa_icon_nd( icon_classes, text: title )
+      icon_and_text_html = fa_icon_nd( icon_classes, text: text )
     else
-      icon_and_text_html = title
+      icon_and_text_html = text
     end
 
     html = ( link_html + icon_and_text_html ).html_safe
