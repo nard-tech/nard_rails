a1950f18e6fa7a5320e78cb2cbf03b7294d68785
Shu Fujita <osorubeki.fujita@gmail.com>
2016-11-25 04:57:58 +0900

    20161125_045800 BugFix - [DecoratorExt] Nard::Rails::DecoratorExt::Datetime

--- lib/nard_rails/decorator_ext/datetime.rb
+++ lib/nard_rails/decorator_ext/datetime.rb
@@ -5,27 +5,31 @@ module Nard::Rails::DecoratorExt::Datetime
 
   included do
 
-    object_class.column_names.each do |column|
-      case object_class.column_type(of: column).to_s
-      when 'datetime'
-        format_name = :normal
-      when 'time'
-        format_name = :only_time_short
-      else
-        format_name = nil
+    unless ENV['SCHEMA_LOAD'].to_s == 'true'
+
+      object_class.column_names.each do |column|
+        case object_class.column_type(of: column).to_s
+        when 'datetime'
+          format_name = :normal
+        when 'time'
+          format_name = :only_time_short
+        else
+          format_name = nil
+        end
+
+        if format_name.present?
+          eval <<-DEF
+            def #{column}
+              if object.#{column}.present?
+                h.l(object.#{column}, format: :#{format_name})
+              else
+                nil
+              end
+            end
+          DEF
+        end
       end
 
-      if format_name.present?
-        eval <<-DEF
-          def #{column}
-            if object.#{column}.present?
-              h.l(object.#{column}, format: :#{format_name})
-            else
-              nil
-            end
-          end
-        DEF
-      end
     end
 
   end
